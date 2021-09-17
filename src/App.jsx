d6f0c05b1f6f4c5ef33771be1f4271d824baa6e0
Christian Thomas <christianthomas@Christians-MacBook-Pro.local>
2021-09-17 15:44:37 -0600

    Added /details route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import BookShelf from './pages/BookShelf';
+import AddBook from './pages/AddBook';
+import Details from './pages/Details';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Router>
-      <h1 style={{ color: 'black' }}>App</h1>
+      <h1>App</h1>
       <Switch>
         <Route exact path="/" component={BookShelf} />
-        <Route path="/bookshelf" component={BookShelf} />
-        <Route path="/add" component={BookShelf} />
-        <Route path="/notfound" component={BookShelf} />
+        <Route path={['/details', '/details/:id']} component={Details} />
+        <Route path="/add" component={AddBook} />
+        <Route path="/notfound" component={NotFound} />
       </Switch>
     </Router>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,3 @@
+const NotFound = () => <h1>404 Resource NotFound</h1>;
+
+export default NotFound;
