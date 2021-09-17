f44f5e01eae5d79330374451a187db9e7bcc63ca
Christian Thomas <christianthomas@Christians-MacBook-Pro.local>
2021-09-17 14:53:54 -0600

    Setup component & page shells.

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const Edit = () => {
+  return (
+    <nav>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Edit;
---
+++ src/pages/AddBook.jsx
@@ -0,0 +1,3 @@
+const AddBook = () => <h1>Add Book</h1>;
+
+export default AddBook;
