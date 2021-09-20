6fd4aae1e73d8de7e625cc6ec96ad6b8a6625d34
Christian Thomas <christianthomas@Christians-MacBook-Pro.local>
2021-09-20 14:13:19 -0600

    Added error boundary and test error.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 
 const App = () => {
+  const testError = new Error('this is a test error!');
+  console.log('error', testError);
+  throw testError;
+
   return (
     <Router>
       <div>
