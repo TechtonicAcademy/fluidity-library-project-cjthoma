c4b68e4204b14f4288e1825e3713be5c2fc90e76
Christian Thomas <christianthomas@Christians-MacBook-Pro.local>
2021-09-20 10:05:37 -0600

    Landing page implemented from original html file.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import '../styles/footer.scss';
+
 const Footer = () => {
   return (
-    <footer>
-      <h1>This is a footer</h1>
+    <footer className="footer">
+      <p className="footer__title">Copy Right 2021 ya boi</p>
     </footer>
   );
 };
-
+//
 export default Footer;
--- src/pages/BookShelf.jsx
+++ src/pages/BookShelf.jsx
@@ -1,3 +1,37 @@
-const BookShelf = () => <h1>BookShelf</h1>;
+import '../styles/bookshelf.scss';
+
+const BookShelf = () => {
+  const testBook = {
+    author: 'Orson Scott Card',
+    title: "Ender's Game",
+    image: 'assets/images/enders_game_cover.jpg',
+    alt: 'enders_game_cover',
+  };
+
+  const books = [];
+
+  for (let i = 0; i < 9; i += 1) {
+    books.push(
+      <div className="main__container__card">
+        <img src={testBook.image} alt={testBook.alt} />
+        <h3>{testBook.title}</h3>
+        <p>{testBook.author}</p>
+      </div>
+    );
+  }
+
+  return (
+    <section className="main">
+      <div className="navbar__searchbar--mobile">
+        <input type="text" placeholder="Search.." />
+        <button type="submit" value="Go">
+          Go
+        </button>
+      </div>
+      <h3 className="main__title">Knowledge is Power!</h3>
+      <section className="main__container">{books}</section>
+    </section>
+  );
+};
 
 export default BookShelf;
