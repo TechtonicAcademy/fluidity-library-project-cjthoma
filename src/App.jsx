500e358c9eb3165867826a963a628d7a4271559d
Christian Thomas <christianthomas@Christians-MacBook-Pro.local>
2021-09-20 08:51:48 -0600

    Fixed router bug.

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,23 @@ import AddBook from './pages/AddBook';
 import Details from './pages/Details';
 import NotFound from './pages/NotFound';
 
-import Header from './components/Header';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={BookShelf} />
-        <Route path="/bookshelf" component={BookShelf} />
-        <Route path={['/details', '/details/:id']} component={Details} />
-        <Route path="/add" component={AddBook} />
-        <Route path="/notfound" component={NotFound} />
-      </Switch>
-      <Footer />
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={BookShelf} />
+          <Route path="/bookshelf" component={BookShelf} />
+          <Route path={['/details', '/details/:id']} component={Details} />
+          <Route path="/add" component={AddBook} />
+          <Route path="/notfound" component={NotFound} />
+        </Switch>
+        <Footer />
+      </div>
     </Router>
   );
 };
