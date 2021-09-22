b7440c8da6631210b84b231e541ea3f13b87f298
Christian Thomas <christianthomas@Christians-MacBook-Pro.local>
2021-09-22 11:14:35 -0600

    Added bookshelf logic and book routing with id param.

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,8 +1,15 @@
-const Book = () => {
+import { NavLink } from 'react-router-dom';
+
+import '../styles/book.scss';
+
+const Book = (props) => {
+  const { title, author, pages, published, synopsis, id} = props.bookData;
   return (
-    <div>
-      <h1>This is a book</h1>
-    </div>
+    <NavLink className="book" to={`/details/${id}`}>
+      <img alt={title.replaceAll(' ', '_') + '_cover'} />
+      <h3>{title}</h3>
+      <p>{author}</p>
+    </NavLink>
   );
 };
 
