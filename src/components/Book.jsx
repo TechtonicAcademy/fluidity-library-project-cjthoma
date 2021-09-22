0734cf5cdacc40f21bd6725de3935825a2a6834d
Christian Thomas <christianthomas@Christians-MacBook-Pro.local>
2021-09-22 14:46:26 -0600

    Added more books and book image support.

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from 'react-router-dom';
-
 import '../styles/book.scss';
 
-const Book = (props) => {
-  const { title, author, pages, published, synopsis, id} = props.bookData;
+const Book = ({ bookData }) => {
+  const { title, author, synopsis, image, id } = bookData; // pages, published, synopsis
+  const imageSrc = getImage(title);
+
   return (
     <NavLink className="book" to={`/details/${id}`}>
-      <img alt={title.replaceAll(' ', '_') + '_cover'} />
+      <img src={imageSrc} alt={image} />
       <h3>{title}</h3>
       <p>{author}</p>
     </NavLink>
@@ -14,3 +15,15 @@ const Book = (props) => {
 };
 
 export default Book;
+
+function getImage (title) {
+  const sourceMap = {
+    "Ender's Game"    : () => { return require('../assets/images/enders_game_cover.jpg') },
+    "The Martian"     : () => { return require('../assets/images/the_martian_cover.jpg') },
+    "Ready Player One": () => { return require('../assets/images/ready_player_one_cover.jpg') },
+    "default"         : () => { return require('../assets/images/enders_game_cover.jpg') },
+  };
+
+  if(!sourceMap[title]) return sourceMap['default']();
+  return sourceMap[title]();
+}
