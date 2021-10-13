4273b339eebe95f5a48bf417d1cf96052afbc4c3
Christian Thomas <christianthomas@Christians-MacBook-Pro.local>
2021-10-13 08:33:44 -0600

    Removed unnecessary variable declarations, changed script main to server.js.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
-const { Author, Book, sequelize } = require('./models');
+const { sequelize } = require('./models');
 
 const app = new express();
 const PORT = process.env.PORT || 8080;
